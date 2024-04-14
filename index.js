c7fa01514180bf71c4db25dab9558b485c35c721
Ermias1995 <ermias.firdawok-ug@aau.edu.et>
2024-04-14 01:57:31 -0700

    all three

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-const leftButton = document.getElementById('left');
-const rightButton = document.getElementById('right');
-let images = [
-"https://www.ancient-origins.net/sites/default/files/field/image/Lalibela.jpg",
-"https://frayedpassport.com/wp-content/uploads/2023/03/gondar-ethiopia-cultural-mosaic-waiting-to-be-explored-frayed-passport.jpg",
-"https://trainsplanesandtuktuks.com/wp-content/uploads/2017/09/Ethiopia-2016-759.jpg",
-"https://i.ytimg.com/vi/_X45yDSVT5w/maxresdefault.jpg"];
-let qoutes = ["Ermiasddsfsdg","sdfdfgdfgdf","cvxbdsfgcxcvb"];
+const qoute = document.getElementById("qoute");
 
-let i=0;
-function goRight(){
-    i+=1;
-    document.querySelector('body').style.background = "url(`${images[i]}`)";
-    document.getElementById('qoute').setAttribute = `${qoutes[i]}`;
+function changeImage1(){
+    document.body.style.background = "url('https://www.ancient-origins.net/sites/default/files/field/image/Lalibela.jpg') center center / cover no-repeat";
+    qoute.innerHTML = "A generation which ignores history has no past and no future.<br><br>Robert Heinlein";
 }
-
+function changeImage2(){
+    document.body.style.background = "url('https://media.istockphoto.com/id/174680666/id/foto/istana-fasiladas-di-gondar-ethiopia.jpg?s=612x612&w=0&k=20&c=HjULwQzHGUrbi55JSSy8h_fwHY930Lv-mSaycK2JhpQ=') center center / cover no-repeat";
+    qoute.innerHTML = "A patriot must always be ready to defend his country against his government.<br><br>Edward Abbey";
+}
+function changeImage3(){
+    document.body.style.background = "url('https://trainsplanesandtuktuks.com/wp-content/uploads/2017/09/Ethiopia-2016-759.jpg') center center / contain";
+    qoute.innerHTML = "A people without knowledge of their past history, origin and culture is like a tree without roots.<br><br> Marcus Garvey";
+}
